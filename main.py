d880ccaaae03dc054d7b3dd08c22cee22851223b
M. Yunus Seker <m.yunusseker@hotmail.com>
2021-12-27 15:45:59 +0300

    train data updated test data and plots are created

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 from sklearn import linear_model
 
 
@@ -9,10 +9,10 @@ class SahibindenPriceEstimator:
         self.test_data = None
         self.model = None
 
-    def read_data(self, file_name, test_size=5):
+    def read_data(self, file_name, train_size=50, test_size=5):
         np.set_printoptions(suppress=True)
         data = np.loadtxt(file_name)
-        self.train_data = data[:-test_size]
+        self.train_data = data[:train_size]
         self.test_data = data[-test_size:]
         print('Data is read')
 
@@ -28,10 +28,40 @@ class SahibindenPriceEstimator:
         for input, price in zip(self.test_data, price_predictions):
             input = input.astype('int')
             print(f"{input[0]}\t{input[1]}\t{input[2]}\t{price}")
+        print(f"Test Data Mean Abs Error: {np.abs(self.test_data[:, 2] - price_predictions).mean()}")
+
+    def plot(self):
+
+        plot_n = 100
+        plot_data = np.zeros((plot_n, 2))
+
+        plot_data[:, 0] = np.linspace(min(self.train_data[:, 0]), max(self.train_data[:, 0]), plot_n)
+        plot_data[:, 1] = np.linspace(max(self.train_data[:, 1]), min(self.train_data[:, 1]), plot_n)
+
+        fig = plt.figure(figsize=(9, 5))
+        plt.scatter(self.train_data[:, 0], self.train_data[:, 2])
+        plt.plot(plot_data[:, 0], self.model.predict(plot_data), 'r-')
+        plt.xlabel('year')
+        plt.ylabel('price')
+        plt.title('Honda Civic Year vs Price Data')
+        plt.legend(['Training Data', 'Regression Model'])
+        plt.savefig('year_price.png', bbox_inches='tight')
+        plt.show()
+
+        fig = plt.figure(figsize=(9, 5))
+        plt.scatter(self.train_data[:, 1], self.train_data[:, 2])
+        plt.plot(plot_data[:, 1], self.model.predict(plot_data), 'r-')
+        plt.xlabel('km')
+        plt.ylabel('price')
+        plt.title('Honda Civic Km vs Price Data')
+        plt.legend(['Training Data', 'Regression Model'])
+        plt.savefig('km_price.png', bbox_inches='tight')
+        plt.show()
 
 
 if __name__ == '__main__':
     estimator = SahibindenPriceEstimator()
-    estimator.read_data('honda_civic.txt', test_size=5)
+    estimator.read_data('honda_civic.txt', train_size=450, test_size=50)
     estimator.train()
     estimator.predict()
+    estimator.plot()
